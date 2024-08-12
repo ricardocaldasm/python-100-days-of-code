5537a7a3771bb3324387d010c515d81765c910fa
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2024-08-12 11:24:01 -0300

    day17

---
+++ day17/class.py
@@ -0,0 +1,23 @@
+# atribute is a variable associated with an object
+class User:
+    def __init__(self, user_id, username) -> None:  # __init__: initialize atributes | None: to allow static type checking (cannot return a int)
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+        # pass -> pass to the next line (like '...')
+    
+    def follow(self, user): # method
+        user.followers += 1 # the user who we're following, their follower count goes up by one
+        self.following += 1 # our own following count goes up by one
+
+
+user_1 = User("001", "Ricardo")
+# user_1.id = "001"
+# user_1.username = "Ricardo"
+print(user_1)
+
+user_2 = User("002","Gaia")
+# user_2.id = "002"
+# user_2.username = "Gaia"
+print(user_2.id, user_2.username, user_2.followers)
