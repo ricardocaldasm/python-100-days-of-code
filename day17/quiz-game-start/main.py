2626446d7b309afbd1306bac53e8a439d6d9abee
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2024-08-14 17:00:43 -0300

    day18

--- day17/quiz-game-start/main.py
+++ day17/quiz-game-start/main.py
@@ -11,7 +11,12 @@ for question in question_data:
     # para passar os atributos da classe Question, são usados question_text e question_answer
     question_bank.append(new_question) # objeto new_question (utilizar .text ou .answer) sendo adicionado à lista question_bank
 
-# print(question_bank[0].text) # show text
+# print(question_bank[0].text) # show list of the question objects (as "object at 0x10f7f2f40")
 
-quiz = QuizBrain(question_bank) # quiz: object
-quiz.next_question()
+quiz = QuizBrain(question_bank) # quiz: object | question_bank is the self.question_list = q_list
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
---
+++ day18/turtle-challange/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+import heroes
+
+tim = Turtle()  # object
+
+tim.shape("turtle")
+tim.color("green")
+for _ in range(4):
+    tim.forward(100)
+    tim.right(90)
+
+screen = Screen()  # object
+screen.exitonclick()
