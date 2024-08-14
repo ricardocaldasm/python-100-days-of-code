ce00a6189670d9ffaefccc463e6be785312862d2
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2024-08-13 21:17:27 -0300

    day17

--- day17/quiz-game-start/main.py
+++ day17/quiz-game-start/main.py
@@ -7,10 +7,11 @@ question_bank = list()
 for question in question_data:
     question_text = question["text"]
     question_answer = question["answer"]
-    new_question = Question(question_text, question_answer) #new_question = object
-    question_bank.append(new_question)
+    new_question = Question(question_text, question_answer)  # new_question = object
+    # para passar os atributos da classe Question, são usados question_text e question_answer
+    question_bank.append(new_question) # objeto new_question (utilizar .text ou .answer) sendo adicionado à lista question_bank
 
 # print(question_bank[0].text) # show text
 
-quiz = QuizBrain(question_bank)
+quiz = QuizBrain(question_bank) # quiz: object
 quiz.next_question()
--- day17/quiz-game-start/question_model.py
+++ day17/quiz-game-start/question_model.py
@@ -1,4 +1,4 @@
 class Question:
     def __init__(self, q_text, q_answer) -> None:
-        self.text = q_text
+        self.text = q_text # attribute
         self.answer = q_answer
