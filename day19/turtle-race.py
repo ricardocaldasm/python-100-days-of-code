e6130ba1afbdd04d2affe32f5f212a85907bbae6
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2024-08-21 15:16:46 -0300

    day19

--- day19/turtle-race.py
+++ day19/turtle-race.py
@@ -9,12 +9,10 @@ y_pos = 150
 
 for turtle_index in range(7):
     turtle_1 = Turtle(shape="turtle")  # instance | they can have different attributes
-# turtle_2 = Turtle(shape="turtle")  # instance
+    # turtle_2 = Turtle(shape="turtle")  # instance
     turtle_1.penup()
     turtle_1.color(colors[turtle_index])
     turtle_1.goto(-200, y_pos)
     y_pos -= 50
-
-
 
 screen.exitonclick()
